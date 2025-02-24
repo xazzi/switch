780011f2f51b3d3f0759a58e93db0d5505c00f67
Bret Combe <bret.combe@gmail.com>
2025-02-24 21:08:58 +0000

    destination bug fix

--- process/support/get-target-height.js
+++ process/support/get-target-height.js
@@ -61,7 +61,7 @@ getTargetHeight = function(s, matInfo, orderArray, data){
         }
 
         // Set the max length for Wixom at 130.
-        if(data.facility = "Wixom"){
+        if(data.facility.destination == "Wixom"){
             if(matInfo.prodName == "Magnet"){
                 if(minimum > 130){
                     minimum = 130;
